d2215b8f3b0bb540bfa5bf563df14812b9d17be5
Hannah C <hannah.cloves@outlook.com>
2020-10-30 20:45:31 +0000

    added in picture of the day to main html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 
 function getTimeStamp(){
     moment().format('LTS');
@@ -6,3 +7,22 @@ function getTimeStamp(){
 function getDateStamp (){
     moment().format('LL');
 }
+
+// AJAX call for picture of the day
+const queryURL = "https://api.nasa.gov/planetary/apod?api_key=TlvltHK45BcgABDzpncHUblvxpst0Cv0BNwk2flA"
+
+// ajax call for UV
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (pictureOfTheDay) {
+    const mainPic = pictureOfTheDay.url
+    const mainPicTitle = pictureOfTheDay.title
+    $("#picHeader").text(mainPicTitle);
+    $('#potdIMG').attr("src", mainPic);
+
+});
+
+
+
+});
