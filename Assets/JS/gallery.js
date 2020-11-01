0ea1b79a65d719a2ed2e103aee14951420e0c4e3
Hannah C <hannah.cloves@outlook.com>
2020-11-01 21:37:42 +0000

    created an object to hold the different image arrays

--- Assets/JS/gallery.js
+++ Assets/JS/gallery.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
-    let apolloImagesArray = [];
+    let completeImagesArray = {apollo: [], moon: [], sun:[], galaxy:[]}
+
+    console.log(completeImagesArray)
     // AJAX call for picture of the day
     const apolloURL = "https://images-api.nasa.gov/search?q=apollo&media_type=image"
     // ajax call for apollo images
@@ -12,14 +14,13 @@ $(document).ready(function () {
             let imageGallery = apolloImages.collection.items[i];
             let imageTitle = imageGallery.data[0].title;
             let imageHref = imageGallery.links[0].href;
-            apolloImagesArray.push({
+            completeImagesArray.apollo.push({
                 href: imageHref,
                 title: imageTitle
             })
         }
         let apolloTitle = apolloImages.collection.items[0].data[0].title;
         let apolloImage = apolloImages.collection.items[0].links[0].href;
-        console.log(apolloImage);
         $("#apolloTitle").text("Title: " + apolloTitle);
         $("#apolloImage").attr("src", apolloImage);
         $("#apolloImage").val(0);
@@ -33,10 +34,20 @@ $(document).ready(function () {
         url: moonURL,
         method: "GET"
     }).then(function (moonImages) {
+        for (let i = 0; i < moonImages.collection.items.length; i++) {
+            let imageGallery = moonImages.collection.items[i];
+            let imageTitle = imageGallery.data[0].title;
+            let imageHref = imageGallery.links[0].href;
+            completeImagesArray.moon.push({
+                href: imageHref,
+                title: imageTitle
+            })
+        }
         let moonTitle = moonImages.collection.items[0].data[0].title
         let moonImage = moonImages.collection.items[0].links[0].href
         $("#moonTitle").text("Title: " + moonTitle);
         $("#moonImage").attr("src", moonImage);
+        $("#moonImage").val(0)
     });
 
     const sunURL = "https://images-api.nasa.gov/search?q=sun&media_type=image"
@@ -45,10 +56,20 @@ $(document).ready(function () {
         url: sunURL,
         method: "GET"
     }).then(function (sunImages) {
+        for (let i = 0; i < sunImages.collection.items.length; i++) {
+            let imageGallery = sunImages.collection.items[i];
+            let imageTitle = imageGallery.data[0].title;
+            let imageHref = imageGallery.links[0].href;
+            completeImagesArray.sun.push({
+                href: imageHref,
+                title: imageTitle
+            })
+        }
         let sunTitle = sunImages.collection.items[0].data[0].title
         let sunImage = sunImages.collection.items[0].links[0].href
         $("#sunTitle").text("Title: " + sunTitle);
         $("#sunImage").attr("src", sunImage);
+        $("#sunImage").val(0);
     });
 
     const galaxiesURL = "https://images-api.nasa.gov/search?q=galaxies&media_type=image"
@@ -57,47 +78,41 @@ $(document).ready(function () {
         url: galaxiesURL,
         method: "GET"
     }).then(function (galaxiesImages) {
+        for (let i = 0; i < galaxiesImages.collection.items.length; i++) {
+            let imageGallery = galaxiesImages.collection.items[i];
+            let imageTitle = imageGallery.data[0].title;
+            let imageHref = imageGallery.links[0].href;
+            completeImagesArray.galaxy.push({
+                href: imageHref,
+                title: imageTitle
+            });
+        }
         let galaxiesTitle = galaxiesImages.collection.items[0].data[0].title
         let galaxiesImage = galaxiesImages.collection.items[0].links[0].href
         $("#galaxiesTitle").text("Title: " + galaxiesTitle);
         $("#galaxiesImage").attr("src", galaxiesImage);
+        $("#galaxiesImage").val(0);
     });
 
 
 
     $(".nextButton").click(function () {
-        console.log("this fired")
         let buttonGroup = $(this).parent().attr("id");
-        console.log(buttonGroup)
-        
-        let apolloImageIndex = parseInt($("#" + buttonGroup + "Image").val());
-
-        console.log($("#" + buttonGroup + "Image").val())
-
-        apolloImageIndex++
-        let nextPhoto = apolloImagesArray[apolloImageIndex];
-        $("#" + buttonGroup + "Title").text("title: " + nextPhoto.title);
-        $("#" + buttonGroup + "Image").attr("src", nextPhoto.href);
-        $("#" + buttonGroup + "Image").val(apolloImageIndex);
-
-
+        let allImageIndex = parseInt($("#" + buttonGroup + "Image").val());
+        allImageIndex++
+        let nextPhoto = completeImagesArray[allImageIndex];
+        $("#" + buttonGroup + "Title").text("title: " + nextPhoto.buttonGroup.title);
+        $("#" + buttonGroup + "Image").attr("src", nextPhoto.buttonGroup.href);
+        $("#" + buttonGroup + "Image").val(allImageIndex);
     });
 
     $(".previousButton").click(function () {
-        console.log("this fired")
         let buttonGroup = $(this).parent().attr("id");
-        console.log(buttonGroup)
-        
-        let apolloImageIndex = parseInt($("#" + buttonGroup + "Image").val());
-
-        console.log($("#" + buttonGroup + "Image").val())
-
-        apolloImageIndex--
-        let nextPhoto = apolloImagesArray[apolloImageIndex];
-        $("#" + buttonGroup + "Title").text("title: " + nextPhoto.title);
-        $("#" + buttonGroup + "Image").attr("src", nextPhoto.href);
-        $("#" + buttonGroup + "Image").val(apolloImageIndex);
-
-
+        let allImageIndex = parseInt($("#" + buttonGroup + "Image").val());
+        allImageIndex--
+        let nextPhoto = completeImagesArray[allImageIndex];
+        $("#" + buttonGroup + "Title").text("title: " + nextPhoto.buttonGroup.title);
+        $("#" + buttonGroup + "Image").attr("src", nextPhoto.buttonGroup.href);
+        $("#" + buttonGroup + "Image").val(alloImageIndex);
     });
 });
