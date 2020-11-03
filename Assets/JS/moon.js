7f04ded63b114593aa4dff7b69fc992901069680
JohnGoodby <70967637+JohnGoodby@users.noreply.github.com>
2020-11-03 18:51:09 +0000

    WIP js change & moon  icons

--- Assets/JS/moon.js
+++ Assets/JS/moon.js
@@ -1,24 +1,52 @@
-const getData =  () => {
-const queryURL = "https://www.icalendar37.net/lunar/api/?";
+
+const queryURL = "https://api.farmsense.net/v1/moonphases/?d=1350526582";
+//console.log(queryURL)
 $.ajax({
-url:queryURL
-})
-};
+    url: queryURL,
+    method: "GET"
 
 
-//function load_moon_phases(obj,callback){
-//    var gets=[]
-//    for (var i in obj){
-//        gets.push(i + "=" +encodeURIComponent(obj[i]))
-//    }
-//    gets.push("LDZ=" + new Date(obj.year,obj.month-1,1) / 1000)
-//    var xmlhttp = new XMLHttpRequest()
-//    var url = "https://www.icalendar37.net/lunar/api/?" + gets.join("&")
-//    xmlhttp.onreadystatechange = function() {
-//        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//            callback(JSON.parse(xmlhttp.responseText))
-//        }
-//    }
-//    xmlhttp.open("GET", url, true)
-//    xmlhttp.send()
-//}
+}).then(function (moonData) {
+    for (let i = 0; i < moonData.collection.items.length; i++) {
+        const element = array[i];
+        
+    }
+}
+
+
+
+    
+)
+.catch();
+
+// }).then(function (apolloImages) {
+//     for (let i = 0; i < apolloImages.collection.items.length; i++) {
+//         let imageGallery = apolloImages.collection.items[i];
+//         let imageTitle = imageGallery.data[0].title;
+//         let imageHref = imageGallery.links[0].href;
+//         completeImagesArray.apollo.push({
+//             href: imageHref,
+//             title: imageTitle
+        })
+    }
+    let apolloTitle = apolloImages.collection.items[0].data[0].title;
+    let apolloImage = apolloImages.collection.items[0].links[0].href;
+    $("#apolloTitle").text(apolloTitle);
+    $("#apolloImage").attr("src", apolloImage);
+    $("#apolloImage").val(0);
+});
+
+moon[0].Moon
+moon[0].Index
+moon[0].Age
+moon[0].Phase
+moon[0].Distance
+moon[0].Illumination
+moon[0].AngularDiameter
+moon[0].DistanceToSun
+moon[0].SunAngularDiameter
+
+const moon = [
+]
+
+var  icon = "Assets\CSS\weather-icons.css" 
