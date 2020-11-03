686078e28e3bbd53f5ccde5a958c1ee441eeeae0
JohnGoodby <70967637+JohnGoodby@users.noreply.github.com>
2020-11-03 20:42:07 +0000

    Initial JS working

--- Assets/JS/moon.js
+++ Assets/JS/moon.js
@@ -1,39 +1,17 @@
-
 const queryURL = "https://api.farmsense.net/v1/moonphases/?d=1350526582";
-//console.log(queryURL)
+console.log(queryURL)
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (moonData) {
+        let moonPhase = moonData[0].Phase
+        console.log(moonPhase)
+        let moonName = moonData[0].Moon[0]
+        console.log(moonName)
+});
 
 
 
-        
-    }
-}
-
-
-
-    
-)
-.catch();
-
-// }).then(function (apolloImages) {
-//     for (let i = 0; i < apolloImages.collection.items.length; i++) {
-//         let imageGallery = apolloImages.collection.items[i];
-//         let imageTitle = imageGallery.data[0].title;
-//         let imageHref = imageGallery.links[0].href;
-//         completeImagesArray.apollo.push({
-//             href: imageHref,
-//             title: imageTitle
-//         })
-//     }
-//     let apolloTitle = apolloImages.collection.items[0].data[0].title;
-//     let apolloImage = apolloImages.collection.items[0].links[0].href;
-//     $("#apolloTitle").text(apolloTitle);
-//     $("#apolloImage").attr("src", apolloImage);
-//     $("#apolloImage").val(0);
-// });
 
 // moon[0].Moon
 // moon[0].Index
@@ -44,8 +22,3 @@ $.ajax({
 // moon[0].AngularDiameter
 // moon[0].DistanceToSun
 // moon[0].SunAngularDiameter
-
-const moon = [
-]
-
-var  icon = "Assets\CSS\weather-icons.css" 
